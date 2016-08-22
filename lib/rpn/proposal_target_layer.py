34eccabf957ed4b00fae6637b8f7e9abd5edb309
yihui-he <yihuihe@foxmail.com>
2016-08-22 16:03:45 +0800

    done proposal target trainable

--- lib/rpn/proposal_target_layer.py
+++ lib/rpn/proposal_target_layer.py
@@ -47,9 +47,15 @@ class ProposalTargetLayer(caffe.Layer):
 
         # Include ground-truth boxes in the set of candidate rois
         zeros = np.zeros((gt_boxes.shape[0], 1), dtype=gt_boxes.dtype)
-        all_rois = np.vstack(
-            (all_rois, np.hstack((zeros, gt_boxes[:, :-1])))
-        )
+        # TRAIN.PROPOSAL_METHOD
+        if cfg.TRAIN.PROPOSAL_METHOD == 'extra':
+            all_rois = np.vstack(
+                (all_rois, np.hstack((zeros, gt_boxes[:, :4])))
+            )
+        else:
+            all_rois = np.vstack(
+                (all_rois, np.hstack((zeros, gt_boxes[:, :-1])))
+            )            
 
         # Sanity check: single batch only
         assert np.all(all_rois[:, 0] == 0), \
