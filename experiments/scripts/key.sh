b42676c5c1c7d9af5962aa4a5e680a8d7a025555
yihui-he <yihuihe@foxmail.com>
2016-08-24 16:41:26 +0800

    test vis

--- experiments/scripts/key.sh
+++ experiments/scripts/key.sh
@@ -53,10 +53,9 @@ esac
 LOG="experiments/logs/key_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
-
+#  --weights data/imagenet_models/${NET}.v2.caffemodel \
 time ./tools/train_net.py --gpu ${GPU_ID} \
   --solver models/${PT_DIR}/${NET}/key/solver.prototxt \
-  --weights data/imagenet_models/${NET}.v2.caffemodel \
   --imdb ${TRAIN_IMDB} \
   --iters ${ITERS} \
   --cfg experiments/cfgs/key.yml \
